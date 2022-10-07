1031e1d675991dc1dd8b115a3b51c32bcb2680a5
Joshua Johns <joshuajohns28@outlook.com>
2022-10-07 09:57:28 -0500

    first

---
+++ main.js
@@ -0,0 +1,6 @@
+const { weaveBag } = require("./bagMaker.js")
+const bag = weaveBag("silk")
+console.log(bag)
+const { createMarble } = require("./marbleMaker.js")
+const marble = createMarble("small")
+console.log(marble)
